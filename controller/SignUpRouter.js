4b94b0b11bc90542ec2053efdbad6a1fb9f3e20c
snehakss <snehakallingal123@gmail.com>
2024-02-09 11:29:29 +0530

    added sign in

--- controller/SignUpRouter.js
+++ controller/SignUpRouter.js
@@ -3,13 +3,42 @@ const user=require("../model/Users")
 
 const router=express.Router()
 
+const bcrypt=require("bcryptjs")
+hashPasswordGenerator=async(pass)=>{
+    const salt=await bcrypt.genSalt(10)
+    return bcrypt.hash(pass,salt)
+}
+
 router.post("/add",async(req,res)=>{
-    let data=req.body
-    let logss=new user(data)
-    let result=await logss.save()
-    res.json({
-        status:"success"
-    })
+   // let data=req.body
+   // let logss=new user(data)
+   // let result=await logss.save()
+   //let name=req.body.name
+   //let age=req.body.age
+   //let phone=req.body.phone
+   //let address=req.body.address
+   //let pincode=req.body.pincode
+   //let emailid=req.body.emailid
+   //let password=req.body.password
+    
+    let {data}={"data":req.body}
+    let password=data.password
+    hashPasswordGenerator(password).then(
+        (hashPassword)=>{
+            console.log(hashPassword)
+            data.password=hashPassword
+            console.log(data)
+            let logss=new user(data)
+            let result= logss.save()
+            res.json({
+                status:"success"
+            })
+        }
+    )
+
+    
 })
+
+
 
 module.exports=router
